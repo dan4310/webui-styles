dcc27c84c153352f261db4d3f37642d97ab856aa
Daniel Courtney <dan4310@gmail.com>
2022-12-13 15:54:48 -0500

    fix(build): fix build & package for modularity

--- gulpfile.js
+++ gulpfile.js
@@ -4,36 +4,53 @@ const sass = require('gulp-sass')(require('sass'))
 const buildDir = './dist'
 const srcDir = './sass'
 const packageDir = './package'
+const isDev = process.env.NODE_ENV === 'development'
 
-function buildBundle(cb, outputStyle = 'compressed') {
+function buildBundle(cb) {
 	src(`./sass/index.scss`)
 		.pipe(
 			sass({
-				outputStyle
+				outputStyle: isDev ? 'expanded' : 'compressed'
 			}).on('error', sass.logError)
 		)
 		.pipe(dest(buildDir))
-
-	src('./sass/fonts/**/**')
-		.pipe(dest(buildDir + '/fonts'))
 	cb()
 }
 
-function dev(cb) {
+function copyFonts(cb, dir = buildDir) {
+	src('./sass/fonts/**/**')
+		.pipe(dest(dir + '/fonts'))
+	cb()
+}
+
+function devWatch(cb) {
 	watch('sass/**/*.scss', { queue: true }, (cb) => buildBundle(cb, 'expanded'))
 	cb()
 }
 
 function package(cb) {
-	src(buildDir + '/**/*.css').pipe(dest(packageDir + '/dist'))
-	src(srcDir + '/**/**.scss').pipe(dest(packageDir + '/sass'))
+	src(buildDir + '/**/*').pipe(dest(packageDir + '/dist'))
+	src(srcDir + '/**/**').pipe(dest(packageDir + '/sass'))
 
 	src(['README.md', 'package.json']).pipe(dest(packageDir))
 	cb()
 }
 
-exports.default = buildBundle
+function buildLibrary(cb) {
+	src('./sass/**/*.scss')
+		.pipe(
+			sass({
+				outputStyle: isDev ? 'expanded' : 'compressed'
+			}).on('error', sass.logError)
+		)
+		.pipe(dest(buildDir))
+	cb()
+}
 
-exports.dev = dev
+exports.default = series(copyFonts, buildLibrary)
 
-exports.package = series(buildBundle, package)
+exports.dev = series(copyFonts, devWatch)
+
+exports.buildDev = series(copyFonts, buildBundle)
+
+exports.package = series(copyFonts, buildLibrary, package)
